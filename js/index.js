c9e290cc163cbc65179c6ff7bcbefc4aa0eb7865
Sergei <sergokondrashevskij@ukr.net>
2021-05-21 16:47:19 +0300

    implemented smoothScroll and tabs

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 import { accordion } from './components/accordion.component.js'
+import { smothScroll } from './components/smothScroll.component.js'
+import { tabs } from './components/tabs.component.js'
 
 document.addEventListener('DOMContentLoaded', () => {
+	smothScroll()
 	accordion()
+	tabs()
 })
